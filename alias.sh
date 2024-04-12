6f3071c9de7f152361cfa92032452ad2833a4fd3
brenden17 <chulwook.jeon@gmail.com>
2024-04-12 14:50:24 +1000

    added

---
+++ alias.sh
@@ -0,0 +1,7 @@
+alias sl='ls'
+alias tt='./dev/my-shell/tmux-split.sh tt && tmux a -t tt'
+alias att='tmux a -t tt'
+alias ctt='tmux kill-session -t tt'
+alias gits='git status'
+alias gitd='git diff'
+
--- tmux-split.sh
+++ tmux-split.sh
@@ -6,13 +6,13 @@ SESSION_NAME=${1:-tt}
 if [ "$SESSION_NAME" = "tt" ]; then
         # create new session
         tmux new -s $SESSION_NAME -d
-        tmux send-keys -t $SESSION_NAME 'cd /var/austlit/app && source ~/.bashrc' C-m
+        tmux send-keys -t $SESSION_NAME 'cd ./dev && source ~/.bashrc' C-m
 
         # create vertical panel for next log panel
         tmux split-window -h -t $SESSION_NAME
-        tmux send-keys -t $SESSION_NAME 'cd /var/austlit/app && source ~/.bashrc' C-m
+        tmux send-keys -t $SESSION_NAME 'cd ./dev && source ~/.bashrc' C-m
         tmux split-window -v -t $SESSION_NAME
-        tmux send-keys -t $SESSION_NAME 'cd /var/austlit/app && source ~/.bashrc' C-m
+        tmux send-keys -t $SESSION_NAME 'cd ./dev && source ~/.bashrc' C-m
         tmux split-window -v -t $SESSION_NAME
-        tmux send-keys -t $SESSION_NAME 'cd /var/austlit/app && source ~/.bashrc' C-m
+        tmux send-keys -t $SESSION_NAME 'cd ./dev && source ~/.bashrc' C-m
 fi
